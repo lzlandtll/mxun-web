3c2bf0e8cf686328b208602c4b206f0c6c8cf699
root <2414896638@qq.com>
2025-03-13 22:29:46 +0800

    注册接口地址更改

--- src/api/login/index.ts
+++ src/api/login/index.ts
@@ -6,10 +6,10 @@ export const accountLogin = (data: any) => {
 }
 
 export const registerAccount = (data: any) => {
-    return request.post({ url: '/open/admin/registerAccount', data: data})
+    return request.post({ url: '/open/sys/registerAccount', data: data})
 }
 
 export const getRegisterSmsCode = (tel:any) => {
-    return request.get({ url: `/open/admin/getRegisterSmsCode?tel=${tel}` })
+    return request.get({ url: `/open/sys/getRegisterSmsCode?tel=${tel}` })
 }
 
